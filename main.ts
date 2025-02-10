39f95f77e041a1554acab385306a3a8c222b6f6f
bomboklada <rada.lahoda08@gmail.com>
2025-02-10 10:29:03 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,32 @@
 //Startovní čára
 Sensors.SetLightLevel()
 radio.setGroup(73)
-let start: boolean = false
+enum State {READY, RUNNING, FINISH}
+let akce: State = State.READY
 let stopwatch: number = 0
+
 radio.onReceivedNumber(function (receivedNumber: number) {
-    if (!start){
-        stopwatch = input.runningTime()
-        whaleysans.showNumber(receivedNumber)
-        start = true
+
+    if (akce === State.FINISH) {
+        akce = State.READY
     }
+
+    if (akce === State.READY){
+        stopwatch = input.runningTime()
+        basic.showNumber(receivedNumber)
+        akce = State.RUNNING
+    }
+
 })
 
 Sensors.OnLightDrop(function() {
-    if (start){
+    if (akce === State.RUNNING){
         let finalTime: number
-        finalTime = input.runningTime() - stopwatch
+        finalTime = (input.runningTime() - stopwatch) / 1000
         //whaleysans.showNumber(input.runningTime())
         radio.sendValue("endTime", finalTime)
-        start = false
+        basic.showNumber(2)
+        akce = State.FINISH
     }
 })
+
